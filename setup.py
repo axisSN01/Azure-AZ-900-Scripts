bf5fe48de84e2c7f2275c86581ca9a899518ad23
Alexis Ibarra <50971046+axisSN01@users.noreply.github.com>
2023-03-16 19:16:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name='azurePyScripts',
+    name='azurepyscripts',
     version='1.0',
     author='Alexis Ibarra',
     description='some auto scripts deployments',
